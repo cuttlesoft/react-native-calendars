32ff1a91019a903d2ff9da989ae2c19d26d2606b
Marisa Gomez <marisa@cuttlesoft.com>
2019-07-10 16:14:23 -0400

    [#167180678] 🚑(CalendarList) Add delay for updating onLayout event
    <description>
    
    - <list-of-completed-items>
    - <list-of-completed-items>
    
    Note: <optional-note>

--- src/calendar-list/index.js
+++ src/calendar-list/index.js
@@ -84,7 +84,9 @@ class CalendarList extends Component {
 
   onLayout(event) {
     if (this.props.onLayout) {
-      this.props.onLayout(event);
+      setTimeout(() => {
+        this.props.onLayout(event);
+    }, 1);
     }
   }
 
